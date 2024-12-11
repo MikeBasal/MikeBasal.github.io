05fc5cf8f7df8585c7ad758c6bb63f1e3cb0dfff
MikeBasal <mtrent0043@kctcs.edu>
2024-12-10 22:46:02 -0500

    Add files via upload

---
+++ WEBSITE_PROJECT_MBT/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+Name: Michael Trent
+File Name: script.js
+Date: 12/9/2024
+*/
+
+
+//Menu
+function toggleMenu() {
+    const navList = document.querySelector('.nav-list');
+    navList.classList.toggle('active');
+}
